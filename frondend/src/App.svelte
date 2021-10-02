e7e70b3f61a059ed1d9d6781102f06895c25f49f
marcschulz <marcleonschulz@gmail.com>
2021-10-02 17:20:35 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	caddyfile
    #	docker-compose.yml

--- frondend/src/App.svelte
+++ frondend/src/App.svelte
@@ -7,7 +7,7 @@
 
     async function getResult() {
 
-        let response = await fetch(`http://localhost:8000/search-${hsn}-${tsn}`,{mode: "no-cors"});
+        let response = await fetch(`http://162.55.208.18:8000/search-${hsn}-${tsn}`);
         let text = await response.json();
         let data = text;
         return data;
@@ -19,17 +19,17 @@
         tsn = "";
     }
 </script>
-<Nav />
-<div style="margin: 60%; margin-top: 5%; margin-left: 15%; margin-right: 15%;" class="grid-container">
+<Nav/>
+<div style="margin: 60%; margin-top: 5%; margin-left: 15%; margin-right: 15%;" className="grid-container">
     <form on:submit|preventDefault={submitHandler}>
-        <p class="form-label">HSN</p>
-        <input bind:value={hsn} class="form-input" type="text" id="input-example-1" placeholder="{tsn}">
-        <p class="form-label">TSN</p>
-        <input bind:value={tsn} class="form-input" type="text" id="input-example-2" placeholder="{tsn}">
-        <button style="margin-top: 2%" class="btn">Suches</button>
+        <p className="form-label">HSN</p>
+        <input bind:value={hsn} className="form-input" type="text" id="input-example-1" placeholder="{tsn}">
+        <p className="form-label">TSN</p>
+        <input bind:value={tsn} className="form-input" type="text" id="input-example-2" placeholder="{tsn}">
+        <button style="margin-top: 2%" className="btn">Suches</button>
     </form>
 
-    {#if result===undefined}
+    {#if result === undefined}
 
         <p></p>
 
@@ -42,11 +42,13 @@
                 {value.error}
             {:else if value.handelsname && value.hersteller_name}
                 <div style="margin-top: 5%;">
-                    <Card css_color={""} header={`Suche für TSN: ${tsn} und TSN: ${hsn}`} main={[`Hersteller :${value.hersteller_name}`,`Handelsname :${value.handel_name}`]}/>
+                    <Card css_color={""} header={`Suche für TSN: ${tsn} und TSN: ${hsn}`}
+                          main={[`Hersteller :${value.hersteller_name}`,`Handelsname :${value.handel_name}`]}/>
                 </div>
             {/if}
         {:catch error}
-            <Card css_color={"text-error"} header={"Error"} main={["Datenbank würd nicht gefunden", `Error Message ="${error.message}"`]}/>
+            <Card css_color={"text-error"} header={"Error"}
+                  main={["Datenbank würd nicht gefunden", `Error Message ="${error.message}"`]}/>
         {/await}
     {/if}
 </div>
